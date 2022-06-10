a06863851ba4f73635c88a13f9303dbda9e0f726
Jack Hughes <jack.hughes@90poe.io>
2022-06-10 01:26:07 +0100

    sleep to test ci

--- scripts/local.sh
+++ scripts/local.sh
@@ -8,6 +8,6 @@ docker-compose up --build -d
 # Get the container ip for the service
 ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' connect)
 echo "container ip: $ip"
-
+sleep 10
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://$ip:8083/connectors/ \
 -d @scripts/debezium/register-postgres.json
